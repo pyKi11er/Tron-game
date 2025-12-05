1b968c41e451a79addc1249a754663d7c0e235f0
Hayk Grigoryan <haykgrigoryan005@gmail.com>
2025-12-05 03:28:20 +0100

    Created Point class

---
+++ src/Point.java
@@ -0,0 +1,15 @@
+package src;
+
+/**
+ *
+ * @author Hayk-PC
+ */
+public class Point {
+    public float x;
+    public float y;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }        
+}
