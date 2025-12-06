e81c06781b184aebd9d550a96bdd2bc7e2d19fb4
Hayk Grigoryan <haykgrigoryan005@gmail.com>
2025-12-06 19:41:10 +0100

    Created the highscore class

---
+++ src/HighScore.java
@@ -0,0 +1,27 @@
+package src;
+
+public class HighScore {
+    
+    private final String name;
+    private final int score;
+
+    public HighScore(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return "HighScore{" + "name=" + name + ", score=" + score + '}';
+    }
+    
+
+}
